b3573b57febe25ab39b1c8066a63dc7afe7f77d4
thomaz <thomazsousa@outook.com.br>
2021-06-22 18:33:07 -0300

    aula 01

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import { View, Text, Image, StatusBar } from 'react-native';
+
+import { styles } from './styles';
+
+import IllustrationImg from '../../assets/illustration.png'
+
+import { ButtonIcon } from '../../components/ButtonIcon';
+export function SignIn(){
+
+	return (
+		<View style={styles.container}>
+			<StatusBar
+				barStyle='light-content'
+				backgroundColor='transparent'
+				translucent
+			/>
+
+			<Image
+				source={IllustrationImg}
+				style={styles.image}
+				resizeMode='stretch'
+			/>
+
+			<View style={styles.content}>				
+				<Text style={styles.title}>
+					{`Organize\nsuas jogatinas\nfacilmente`}
+				</Text>
+
+				<Text style={styles.subtitle}>
+					Crie grupos para jogar seus games{`\n`}favoritos com seus amigos
+				</Text>
+
+				<ButtonIcon title='Entrar com o Discord'activeOpacity={0.7}/>
+			</View>
+		</View>
+	);
+}
